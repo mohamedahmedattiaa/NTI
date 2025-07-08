6b099d94305bda38d8b5abdd15709b2e4683179e
Mohamed AHmed Attia <mohamedahmedattia27@gmail.com>
2025-07-08 14:42:40 +0300

    Handle Numerical Value

--- Day2/Task3/index.js
+++ Day2/Task3/index.js
@@ -1,5 +1,10 @@
-// Question 3
-let message2 = prompt("Enter the word to check how many times 'e' is repeated:");
+let message2 = prompt(
+  "Enter the word to check how many times 'e' is repeated:"
+);
+if (/^\d+$/.test(message2)){
+  alert("This a Numerical value");
+}
+
 let count = 0;
 for (let i = 0; i < message2.length; i++) {
   if (message2[i] === "e") {
